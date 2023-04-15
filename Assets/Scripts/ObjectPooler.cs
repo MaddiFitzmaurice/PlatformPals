fbf953d50c955d9253e297db34598ae28ced01c9
Maddi <maddi.fitzmaurice@gmail.com>
2023-04-15 21:28:19 +1000

    Add and Delete Platforms
    - Set up basic add and delete platform mechanic using left and right mouse click

---
+++ Assets/Scripts/ObjectPooler.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ObjectPooler
+{
+    // Create object pool using a prefab and amount to pool
+    public static List<GameObject> CreateObjectPool(int amountToPool, GameObject objectToPool)
+    {
+        List<GameObject> pooledObjects = new List<GameObject>();
+
+        for (int i = 0; i < amountToPool; i++) 
+        {
+            GameObject obj = GameObject.Instantiate(objectToPool);
+            obj.SetActive(false);
+            pooledObjects.Add(obj);
+        }
+
+        return pooledObjects;
+    }
+
+    //Retrieve a pooled object if it is not active in the hierarchy
+    public static GameObject GetPooledObject(List<GameObject> pooledObjects)
+    {
+        foreach (GameObject obj in pooledObjects)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                return obj;
+            }
+        }
+
+        return null;
+    }
+
+    // Return all objects to pool
+    public static void ReturnObjectsToPool(List<GameObject> pooledObjects)
+    {
+        foreach (GameObject obj in pooledObjects)
+        {
+            if (obj.activeInHierarchy)
+            {
+                obj.SetActive(false);
+            }
+        }
+    }
+
+    // Assign parent transform for pooled objects to organise hierarchy
+    public static void AssignParentGroup(List<GameObject> pooledObjects, Transform parent)
+    {
+        for (int i = 0; i < pooledObjects.Count; i++)
+        {
+            pooledObjects[i].transform.parent = parent;
+        }
+    }
+}
---
+++ Assets/Scripts/PlatformManager.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformManager : MonoBehaviour
+{
+    // Platform type and maximum number to spawn in level
+    [SerializeField] private GameObject _platformPrefab;
+    [SerializeField] private int _maxPlatforms;
+
+    private List<GameObject> _platforms; 
+
+    void Start()
+    {
+        _platforms = ObjectPooler.CreateObjectPool(_maxPlatforms, _platformPrefab);
+        ObjectPooler.AssignParentGroup(_platforms, this.transform);
+    }
+
+    void Update()
+    {
+        // Place platform on left click
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject platform = ObjectPooler.GetPooledObject(_platforms);
+
+            // Check that there's platforms left in the pool before placing
+            if (platform != null)
+            {
+                // Grab mouse pos and add 1 to z so platform is behind clipping plane
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                mousePos.z += 1;
+
+                // Spawn platform
+                platform.transform.position = mousePos;
+                platform.SetActive(true);
+            }
+        }
+
+        // Delete platform on right click
+        if (Input.GetMouseButtonDown(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);   
+            RaycastHit hitInfo;
+
+            // if mouse clicks on a platform
+            if (Physics.Raycast(ray, out hitInfo))
+            {
+                if (hitInfo.collider.CompareTag("Platform"))
+                {
+                    hitInfo.collider.gameObject.SetActive(false);
+                }
+            }
+        }
+
+        
+    }
+
+}
