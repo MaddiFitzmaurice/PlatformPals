b721bafbdff07d66cd1bcf72360e639e8a2b99d9
JR14141414 <128942924+JR14141414@users.noreply.github.com>
2023-04-17 21:51:05 +1000

    PlayerMovement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private LayerMask jumpableGround;
+    private Rigidbody rb;
+    [SerializeField] public float speed;
+    [SerializeField] public float maxSpeed;
+    [SerializeField] public float acceleration;
+
+    [SerializeField] public float minJump;
+    [SerializeField] public float maxJump;
+
+    private SpriteRenderer spriteRenderer;
+
+    private float inputAxis;
+    public bool isGrounded = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        HorizontalMovement();
+
+        if (isGrounded)
+        {
+            GroundedMovement();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ContactPoint contactPoint = collision.contacts[0];
+        isGrounded = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        isGrounded = false;
+    }
+
+    private void HorizontalMovement()
+    {
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        if (speed < 0)
+        {
+            speed = 0;
+        }
+        if ((speed < maxSpeed) && (horizontalInput != 0))
+        {
+            speed = speed + acceleration * Time.deltaTime;
+        }
+        else if ((speed > 0) && (horizontalInput == 0))
+        {
+            speed = speed - acceleration * Time.deltaTime;
+        }
+
+            rb.velocity = new Vector3(horizontalInput * speed, rb.velocity.y, 0);
+    }
+
+    private void GroundedMovement()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            Jump();
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, maxJump, 0f);
+    }
+}
