e934df165006893ce509c21ada53e542464a6c49
Maddi <maddi.fitzmaurice@gmail.com>
2023-04-17 21:35:06 +1000

    Platform Outline
    - Added outline that follows mouse to help player place platforms in level

--- Assets/Scripts/PlatformManager.cs
+++ Assets/Scripts/PlatformManager.cs
@@ -7,6 +7,7 @@ public class PlatformManager : MonoBehaviour
     // Platform type and maximum number to spawn in level
     [SerializeField] private GameObject _platformPrefab;
     [SerializeField] private int _maxPlatforms;
+    [SerializeField] private GameObject _platformOutline;
 
     private List<GameObject> _platforms; 
 
@@ -14,10 +15,13 @@ public class PlatformManager : MonoBehaviour
     {
         _platforms = ObjectPooler.CreateObjectPool(_maxPlatforms, _platformPrefab);
         ObjectPooler.AssignParentGroup(_platforms, this.transform);
+        _platformOutline.SetActive(true);
     }
 
     void Update()
     {
+        _platformOutline.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition) + new Vector3(0, 0, 1f);
+
         // Place platform on left click
         if (Input.GetMouseButtonDown(0))
         {
