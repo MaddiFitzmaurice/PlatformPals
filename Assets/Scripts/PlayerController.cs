55645b85d2a90172e5dabe9b4a0010854e0dea34
JR141414 <100816099+JR141414@users.noreply.github.com>
2023-04-19 15:57:10 +1000

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,79 +4,109 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] private LayerMask jumpableGround;
-    private Rigidbody rb;
-    [SerializeField] public float speed;
-    [SerializeField] public float maxSpeed;
-    [SerializeField] public float acceleration;
+    //Component Declaration
+    [SerializeField] private Rigidbody rb;
 
-    [SerializeField] public float minJump;
-    [SerializeField] public float maxJump;
+    //Input Declaration
+    private float horizontalInput;
 
-    private SpriteRenderer spriteRenderer;
+    //Object State Declaration
+    public bool canJump;
+    public bool attemptJump;
+    public bool grounded;
+    public bool isFacingRight;
 
-    private float inputAxis;
-    public bool isGrounded = true;
+    //Check Declaration
+    [SerializeField] private Transform feet;
+    private float timeOffGround;
+    [SerializeField] private float coyoteTime;
 
-    void Start()
+    //Layer Declaration
+    [SerializeField] private LayerMask groundLayer;
+
+    //Player Stat Declaration
+    [SerializeField] private float groundSpeed;
+    [SerializeField] private float airSpeed;
+    [SerializeField] private float acceleration;
+    [SerializeField] private float deceleration;
+    [SerializeField] private float jumpForce;
+
+    private void Awake()
     {
         rb = GetComponent<Rigidbody>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-    void Update()
+    private void Start()
     {
-        HorizontalMovement();
+        
+    }
 
-        if (isGrounded)
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            GroundedMovement();
+            attemptJump = true;
+            
+        }
+        
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            attemptJump = false;
+        }
+
+        if (Physics.Raycast(feet.position, -Vector2.up, 0.1f, groundLayer))
+        {
+            grounded = true;
+            canJump = true;
+        }
+        else
+        {
+            grounded = false;
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    private void FixedUpdate()
     {
-        ContactPoint contactPoint = collision.contacts[0];
-        isGrounded = true;
-    }
+        Move();
 
-    private void OnCollisionExit(Collision collision)
-    {
-        isGrounded = false;
-    }
-
-    private void HorizontalMovement()
-    {
-        float horizontalInput = Input.GetAxisRaw("Horizontal");
-        if ((speed < maxSpeed) && (speed > -maxSpeed) && (horizontalInput != 0))
-        {
-            speed = speed + horizontalInput * acceleration * Time.deltaTime;
-        }
-        else if (horizontalInput == 0)
-        {
-            if (speed > 0)
-            {
-                speed = speed - acceleration * Time.deltaTime;
-            }
-            if (speed < 0)
-            {
-                speed = speed + acceleration * Time.deltaTime;
-            }
-        }
-
-            rb.velocity = new Vector3(speed, rb.velocity.y, 0);
-    }
-
-    private void GroundedMovement()
-    {
-        if(Input.GetKey(KeyCode.Space))
+        if ((attemptJump) && (canJump))
         {
             Jump();
         }
+
+        if ((!attemptJump) && (!grounded))
+        {
+            //CutJump();
+        }
     }
 
-    private void Jump()
+    public void Jump()
     {
-        rb.velocity = new Vector3(rb.velocity.x, maxJump, 0f);
+        rb.AddForce(jumpForce * Vector3.up, ForceMode.Impulse);
+        canJump = false;
+        timeOffGround = 0f;
+    }
+
+    public void Move()
+    {
+        float targetVelocity;
+        if (grounded)
+        {
+            targetVelocity = groundSpeed * horizontalInput;
+        }
+        else
+        {
+            targetVelocity = airSpeed * horizontalInput;
+        }
+
+        float velocityDifference = targetVelocity - rb.velocity.x;
+
+        float accelerationRate = (Mathf.Abs(targetVelocity) > 0.01f) ? acceleration : deceleration;
+
+        float movement = Mathf.Abs(velocityDifference) * acceleration * Mathf.Sign(velocityDifference);
+
+        rb.AddForce(movement * Vector3.right);
     }
 }
