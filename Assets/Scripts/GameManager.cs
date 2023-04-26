fd40f193366b1719594c0ad379d504b542743a7f
Maddi <maddi.fitzmaurice@gmail.com>
2023-04-26 18:03:09 +1000

    GameManager Fix

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+#if UNITY_EDITOR
 using UnityEditor;
+#endif
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -72,7 +74,9 @@ public class GameManager : MonoBehaviour
         }
         else
         {
+#if UNITY_EDITOR
             EditorApplication.ExitPlaymode();
+#endif
         }
     }
 
