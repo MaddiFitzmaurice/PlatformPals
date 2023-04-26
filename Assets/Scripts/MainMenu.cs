70413e176399f9812b05df6d3db37994b9eff242
Maddi <maddi.fitzmaurice@gmail.com>
2023-04-26 21:19:02 +1000

    Menu + Music

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,13 +6,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject _mainMenu;
+    [SerializeField] private GameObject _instructions;
+
     void Start()
     {
         Cursor.visible = true;
+        _mainMenu.SetActive(true);
+        _instructions.SetActive(false);
     }
 
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    public void InstructionsActive(bool flag)
+    {
+        _instructions.SetActive(flag);
+        _mainMenu.SetActive(!flag);
     }
 }
