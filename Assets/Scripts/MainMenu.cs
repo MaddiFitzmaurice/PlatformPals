80d869677a54468b338cbc7945478824822afb94
Maddi <maddi.fitzmaurice@gmail.com>
2023-04-26 17:31:27 +1000

    Main Menu Bug Fix

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,6 +6,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    void Start()
+    {
+        Cursor.visible = true;
+    }
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
